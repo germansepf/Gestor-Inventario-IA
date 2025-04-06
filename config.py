9762b0ceaa7d56c66d311eb8323685770018ac9c
germansepf <gersepez@gmail.com>
2025-04-05 23:58:40 -0500

    config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 
 # Variables de entorno para las APIs (opcional, si no quieres leerlas en otros archivos)
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyBv1XNHwjFe5SvzkPK0PN1kf1hhzA_QF_c")
-GOOGLE_CX = os.getenv("GOOGLE_CX", "54361eb35c4e04805")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "TU-API-KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "TU-API")
+GOOGLE_CX = os.getenv("GOOGLE_CX", "API")
 
 # Ajustes para la generación de descripciones
 OPENAI_ENGINE = "gpt-4o"
